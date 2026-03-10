a2c2f19806beb87db528b7f8213590149d612432
chio <chio.orlands@gmail.com>
2026-03-10 22:36:42 +0700

    modified:   app/Http/Controllers/Transaction/GudangController.php

--- app/Http/Controllers/Transaction/GudangController.php
+++ app/Http/Controllers/Transaction/GudangController.php
@@ -1387,25 +1387,28 @@ public function submit(Request $request)
                     );
                 }
                 //3.8
-                if ($qtyraw <= 0) {
-                    $qty = 0;
-                }
-                elseif ($rounddosage) {
-                    if ($qtyraw <= 0.05) {
+                // ✅ ROUNDING HANYA SEKALI (sama seperti di view)
+                if ($rounddosage) {
+                    // Kelipatan 0.05
+                    if ($qtyraw <= 0) {
+                        $qty = 0;
+                    } elseif ($qtyraw <= 0.05) {
                         $qty = 0.05;
                     } else {
-                        $qty = ceil($qtyraw / 0.05) * 0.05;
+                        $truncated = floor($qtyraw * 100) / 100;
+                        $qty = ceil($truncated / 0.05) * 0.05;
                     }
-                }
-                else {
-                    if ($qtyraw <= 0.01) {
+                } else {
+                    // Kelipatan 0.01 (2 desimal biasa)
+                    if ($qtyraw <= 0) {
+                        $qty = 0;
+                    } elseif ($qtyraw <= 0.01) {
                         $qty = 0.01;
                     } else {
-                        $qty = ceil($qtyraw / 0.01) * 0.01;
+                        $qty = round($qtyraw, 2);
                     }
                 }
 
-                $qty = round($qty,2);
                 Log::info('ROUNDING_DEBUG',[
                     'itemcode'=>$itemcode,
                     'qtyraw'=>$qtyraw,
